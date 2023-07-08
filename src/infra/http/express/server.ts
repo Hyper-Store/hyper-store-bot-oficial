b9e3123062cb34be84a92aa725b4392847b8d798
rethred <gabrielwastha@gmail.com>
2023-07-08 18:54:16 -0300

    insomia

--- src/infra/http/express/server.ts
+++ src/infra/http/express/server.ts
@@ -2,15 +2,18 @@ import { CreateMercadopagoPaymentUsecase } from "@/modules/payment/providers/mer
 import express from "express"
 
 const app = express()
+app.use(express.json())
 
-app.post("payment/mercadopago/callback", async (req, res) => {
-    console.log("hello world")
-    await new CreateMercadopagoPaymentUsecase().execute({
-        mercadopagoPaymentId: req.body.id
-    })
+app.post("/payment/mercadopago/callback", async (req, res) => {
+    console.log(req.body)
+
+    // await new CreateMercadopagoPaymentUsecase().execute({
+    //     mercadopagoPaymentId: req.body.id
+    // })
 
     res.status(200).json({ ok: true })
 })
+
 
 const PORT = 5000
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
