0e3fee1626d253147638f4b3100c3a4b59c6b77d
Daniel-D3V <contatodanielsilvaoficial@gmail.com>
2023-07-08 16:12:23 -0300

    save

---
+++ src/modules/purchases/events/checkout/finish-payment/mercadopago/finish-payment-mercado-pago.event.ts
@@ -0,0 +1,34 @@
+import { BaseEvent } from "@/modules/@shared/domain";
+import { Interaction } from "discord.js";
+import Discord, { Client } from "discord.js"
+import { Database } from "@/infra/app/setup-database";
+import { ProductType } from "@/modules/purchases/@types/Product.type";
+import { colors } from "@/modules/@shared/utils/colors";
+import { emojis } from "@/modules/@shared/utils/emojis";
+import { CheckoutType } from "../../@shared/_types/Checkout.type";
+
+class FinishPaymentMercadoPagoPurchasesEvent extends BaseEvent {
+    constructor() {
+        super({
+            event: "interactionCreate"
+        })
+    }
+
+    async exec(interaction: Interaction, client: Client): Promise<void> {
+        if (!interaction.isStringSelectMenu()) return;
+        if (interaction.customId !== 'method-payment') return;
+        if (interaction.values[0] !== "mercadopago") return;
+
+        const checkout = await new Database().get(`purchases.checkouts.${interaction.channelId}`) as CheckoutType;
+        const product: ProductType | undefined = await new Database().get(`purchases.products.${checkout.productId}`) as ProductType;
+
+        if (interaction.user.id !== checkout.ownerId) return;
+
+        console.log('ta aq')
+    }
+}
+
+export default (client: Client): void => {
+    const buttonClickedEvent = new FinishPaymentMercadoPagoPurchasesEvent()
+    buttonClickedEvent.setupConsumer(client)
+}
