5d75c4ec571a0f28ad932c1486bb71bbe6c06c6e
Daniel-D3V <contatodanielsilvaoficial@gmail.com>
2023-07-08 21:21:56 -0300

    save

--- src/modules/product/@shared/workers/update-message-product/index.ts
+++ src/modules/product/@shared/workers/update-message-product/index.ts
@@ -1,22 +1,22 @@
 import { Database } from "@/infra/app/setup-database"
-import { ProductType } from "@/modules/purchases/@types/Product.type";
 import Discord, { Interaction } from "discord.js"
 import { ProductMessage } from "../../messages/product-message/product-message";
+import { ProductRepository } from "@/modules/product/repositories/product.repository";
 
 export const UpdateMessageProduct = async (interaction: Interaction, productId: string): Promise<void> => {
-    const db_product: ProductType | undefined = await new Database().get(`purchases.products.${productId}`) as ProductType;
+    const product = await ProductRepository.findById(productId);
 
-    const channel = interaction.guild?.channels.cache.get(db_product.channelId!);
+    const channel = interaction.guild?.channels.cache.get(product?.channelId!);
     if (channel?.type !== Discord.ChannelType.GuildText) return;
 
-    const message = channel?.messages.cache.get(db_product.messageId!);
+    const message = channel?.messages.cache.get(product?.messageId!);
 
     if (!channel || !message) {
-        delete db_product.messageId
-        delete db_product.channelId
-        new Database().set(`purchases.products.${productId}`, db_product);
+        delete product?.messageId
+        delete product?.channelId
+        new Database().set(`purchases.products.${productId}`, product!);
         return;
     }
 
-    message.edit(await ProductMessage(interaction, db_product));
+    message.edit(await ProductMessage(interaction, product!));
 }
